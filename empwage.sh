2260339e427b1f7e4bee4a335d94493a4f2af241
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-07-08 16:39:22 +0530

    Added:salary cal using case statement

--- empwage.sh
+++ empwage.sh
@@ -4,19 +4,24 @@ parttime=2
 randomcheck=$((RANDOM%3))
 wagePerHr=20
 
-if [ $fulltime -eq $randomcheck ]
-then
-	empHrs=8
-	salary=$((empHrs*wagePerHr))
-	echo "Employee salary is $salary"
-elif [ $parttime -eq $randomcheck ]
-then
-	empHrs=8
-	salary=$((empHrs*wagePerHr))
-	echo "Employee part time salary is $salary"
-else
-	empHrs=0
-	salary=0
-	echo "employee is absent, so salary is $salary"
-fi
+case $randomcheck in
+$fulltime)
+empHrs=8
+salary=$((wagePerHr*empHrs))
+echo "salary of employee is $salary"
+;;
+$partime)
+empHrs=8
+salary=$((wagePerHr*empHrs))
+echo "salary of part time employee is $salary"
+;;
+*)
+empHrs=0
+salary=0
+echo "employee is absent"
+;;
+esac
+
+
+
 
