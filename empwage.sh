cf026270edb187a59945fee9931e917f7a4f0f8d
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-07-08 16:04:15 +0530

    Added:emplyee attendance

--- empwage.sh
+++ empwage.sh
@@ -1,1 +1,9 @@
-#welcome to employee wage computation
+#!/bin/bash -x
+fulltime=1
+randomcheck=$((RANDOM%2))
+if [ $fulltime -eq $randomcheck ]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
