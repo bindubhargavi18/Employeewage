14c55d4479c6dd19aa0e28d6ba3e5a7614d2d5ed
bindubhargavi18 <bindubhargavi568@gmail.com>
2021-07-08 16:27:31 +0530

    added:employee part time

--- empwage.sh
+++ empwage.sh
@@ -1,12 +1,22 @@
 #!/bin/bash -x
 fulltime=1
-randomcheck=$((RANDOM%2))
+parttime=2
+randomcheck=$((RANDOM%3))
 wagePerHr=20
-empHrs=8
 
 if [ $fulltime -eq $randomcheck ]
 then
-	salary=$((wagePerHr*empHrs))
+	empHrs=8
+	salary=$((empHrs*wagePerHr))
+	echo "Employee salary is $salary"
+elif [ $parttime -eq $randomcheck ]
+then
+	empHrs=8
+	salary=$((empHrs*wagePerHr))
+	echo "Employee part time salary is $salary"
 else
+	empHrs=0
 	salary=0
+	echo "employee is absent, so salary is $salary"
 fi
+
